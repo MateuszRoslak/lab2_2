b276bea569b17bafd54c76c072c5901e8eeb9f2a
MateuszRoslak <222554@edu.p.lodz.pl>
2021-03-22 18:56:00 +0100

    sequence tests

--- src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
+++ src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
@@ -1,14 +1,45 @@
 package edu.iis.mto.similarity;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
 
+import edu.iis.mto.searcher.SearchResult;
+import edu.iis.mto.searcher.SequenceSearcher;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class SimilarityFinderTest {
+    SimilarityFinder finder = new SimilarityFinder(new SequenceSearcher() {
+        @Override
+        public SearchResult search(int elem, int[] sequence) {
+            for (int i = 0; i < sequence.length; i++) {
+                if(sequence[i] == elem){
+                    return SearchResult.builder().withFound(true).withPosition(i).build();
+                }
+            }
+            return SearchResult.builder().withFound(false).build();
+        }
+    });
 
     @Test
-    void test() {
-        fail("Not yet implemented");
+    public void equalSequencesTest() {
+        int[] seq1 = {0,1,2,3};
+        int[] seq2 = {0,1,2,3};
+        assertEquals(1, finder.calculateJackardSimilarity(seq1, seq2));
     }
 
+    @Test
+    public void notEqualSequencesTest() {
+        int[] seq1 = {0,1,4,5};
+        int[] seq2 = {6,7,8,9};
+        assertEquals(0.0, finder.calculateJackardSimilarity(seq1, seq2));
+    }
+
+    @Test
+    public void halfEqualSequencesTest() {
+        int[] seq1 = {0,1,2,3};
+        int[] seq2 = {0,1};
+        assertEquals(0.5, finder.calculateJackardSimilarity(seq1, seq2));
+    }
 }
